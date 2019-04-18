3933a62f66b6b3f837fd6f00058ce60a9736f7f3
Adam Grohs <adamgrohs@Adams-MacBook-Air.local>
2019-04-18 13:40:41 -0500

    chore: code & tags cleanup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const { json, send } = require('micro')
 const shippo = require('shippo')(process.env.SHIPPO_PRIVATE_KEY)
-const AWS = require('aws-sdk')
 const { createClient } = require('@moltin/request')
+const AWS = require('aws-sdk')
 const SES = new AWS.SES({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
